ad4fec26591ebdb5ef35c83d43bd51e05693a043
Mikael Korpela <mikael@ihminen.org>
2015-01-25 00:02:01 +0200

    Hide links for adding new content from the sidebar

--- resources/js/ext.HitchwikiVector.js
+++ resources/js/ext.HitchwikiVector.js
@@ -1,9 +1,19 @@
 ( function ( mw, $ ) {
 
-  // For some odd reason, these had fixed min-style:600px
-  // That sucks. Removing it (they're handled at HitchwikiVector/resources/styles/forms.less instead)
 	$( function () {
+
+    // For some odd reason, these had fixed min-style:600px
+    // That sucks. Removing it (they're handled at HitchwikiVector/resources/styles/forms.less instead)
     $(".sf-select2-container").attr("style", "");
+
+    // Don't allow adding new content for non logged in users
+    // wgUserId returns null when not logged in
+    if(!mw.config.get('wgUserId')) {
+	    $("#n-New-spot").hide();
+		  $("#n-New-city").hide();
+		  $("#n-Add-photo").hide();
+	  }
+
 	} );
 
 }( mediaWiki, jQuery ) );
