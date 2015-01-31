30384b5b99fc4d1865f091d3d2d7c29b73bfe835
Mikael Korpela <mikael@ihminen.org>
2015-01-31 22:43:09 +0200

    Interwiki / languages bar

--- resources/js/ext.HitchwikiVector.languages.js
+++ resources/js/ext.HitchwikiVector.languages.js
@@ -3,15 +3,9 @@
 
   $languagesPortal = $("#p-lang");
 
-  $languages = $languagesPortal.find("ul");
+  $languages = $languagesPortal.find("ul").addClass("hw-interwiki");
 
-  //$languagesWrap = $('<div id="hw-language"><a href="#" id="hw-language-dropdown">In other languages</a></div>');
-  $languagesWrap = $('<li class="mw-icon mw-more-icon mw-menu-item" id="mw-language">In other languages</li>');
-
-  $languages.addClass('mw-more-menu');
-  $languages.find("li").addClass('mw-menu-item');
-  $languagesWrap.append($languages);
-  $languagesWrap.appendTo("#mw-page-actions");
+  $languages.insertAfter("#firstHeading");
 
   $languagesPortal.remove();
 
