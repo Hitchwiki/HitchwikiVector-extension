b1e624523a113e76200d119ebdc9b0d471d7d7cf
Mikael Korpela <mikael@ihminen.org>
2016-12-07 18:27:31 +0200

    Use singlequotes at js

--- resources/js/ext.HitchwikiVector.languages.js
+++ resources/js/ext.HitchwikiVector.languages.js
@@ -3,21 +3,21 @@
 
   if(mw.config.get('wgAction') == 'view') {
 
-    $languagesPortal = $("#p-lang");
+    $languagesPortal = $('#p-lang');
 
     if($languagesPortal.length) {
 
-      $languages = $languagesPortal.find("ul").addClass("hw-interwiki");
+      $languages = $languagesPortal.find('ul').addClass('hw-interwiki');
 
       // Add languages under the main title on other pages except main page
       if(mw.config.get('wgPageName') != 'Main_Page') {
-        $languages.insertAfter("#firstHeading");
+        $languages.insertAfter('#firstHeading');
       }
 
       $languagesPortal.remove();
 
       if(mw.config.exists( 'wgUserLanguage' )) {
-        $languages.find( ".interwiki-" + mw.config.get('wgUserLanguage') ).addClass("hw-userlang");
+        $languages.find( '.interwiki-' + mw.config.get('wgUserLanguage') ).addClass('hw-userlang');
       }
 
     }
