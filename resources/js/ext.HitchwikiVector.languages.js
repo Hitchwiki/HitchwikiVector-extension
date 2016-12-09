bc10a17f48cf303bcf3755d4ea715ec6700fe741
Mikael Korpela <mikael@ihminen.org>
2016-12-09 16:34:48 +0200

    Add comments to js

--- resources/js/ext.HitchwikiVector.languages.js
+++ resources/js/ext.HitchwikiVector.languages.js
@@ -3,21 +3,25 @@
 
   if(mw.config.get('wgAction') == 'view') {
 
+    // Languages list at the sidebar
+    // Produced by `LANGUAGES` at `/scripts/pages/MediaWiki:Sidebar`
     $languagesPortal = $('#p-lang');
 
     if($languagesPortal.length) {
 
+      // Grab languages list
       $languages = $languagesPortal.find('ul').addClass('hw-interwiki');
 
       // Add languages under the main title on other pages except main page
-      if(mw.config.get('wgPageName') != 'Main_Page') {
+      if(mw.config.exists('wgPageName') && mw.config.get('wgPageName') != 'Main_Page') {
         $languages.insertAfter('#firstHeading');
       }
 
+      // Remove languages list from the sidebar
       $languagesPortal.remove();
 
       // Highlight users own language
-      if(mw.config.exists( 'wgUserLanguage' )) {
+      if(mw.config.exists('wgUserLanguage')) {
         $languages.find( '.interwiki-' + mw.config.get('wgUserLanguage') ).addClass('hw-userlang');
       }
 
