fe8a5395beb337789342499099f8fe4dd3ea63fd
Mikael Korpela <mikael@ihminen.org>
2016-12-09 16:42:56 +0200

    Replace tabs by spaces

--- includes/tagLanguages.php
+++ includes/tagLanguages.php
@@ -14,14 +14,14 @@
  *         (
  *             [en] => English
  *             [de] => German
- * 						...
+ *             ...
  *             [zh] => Chinese
  *         )
  *
  *     [wikinames] => Array
  *         (
  *             [de] => Tramperwiki
- * 						...
+ *             ...
  *             [pt] => CaronaWiki
  *         )
  *
@@ -31,7 +31,7 @@
  *                 (
  *                     [0] => nl
  *                     [1] => lt
- * 										...
+ *                     ...
  *                     [8] => bg
  *                 )
  *
@@ -64,47 +64,47 @@ class HitchwikiVectorTagLanguages {
   static function languagesRender( $input, array $args, Parser $parser, PPFrame $frame ) {
     global $hwLangConfig;
 
-		if(!is_array($hwLangConfig["languages"]) || !isset($hwLangConfig['settings']['baby'])) {
-			return '';
-		}
+    if(!is_array($hwLangConfig["languages"]) || !isset($hwLangConfig['settings']['baby'])) {
+      return '';
+    }
 
-		$languages = '<div class="mw-languages">';
-
-
-		// Big wikis
-		$languages .= '<ul class="mw-languages-major">';
-  	foreach($hwLangConfig["languages"] as $langCode => $langName) {
-			// If it's NOT in Babylist
-			if(!in_array($langCode, $hwLangConfig['settings']['baby'])) {
-
-				$languages .= '<li>';
-				$languages .= '<a href="/'.$langCode.'">'.$langName.'</a>';
-				$languages .= '<span class="hw-languages-articles">' . sprintf('%d articles', 0) . '</span>';
-				$languages .= '</li>';
-			}
-		}
-		$languages .= '</ul>';
+    $languages = '<div class="mw-languages">';
 
 
-		// Baby wikis
-		$languages .= '<h4 class="mw-languages-label-minor">Baby wikis</h4>';
-		$languages .= '<ul class="mw-languages-minor">';
-  	foreach($hwLangConfig["languages"] as $langCode => $langName) {
-			// If it IS in Babylist
-			if(in_array($langCode, $hwLangConfig['settings']['baby'])) {
+    // Big wikis
+    $languages .= '<ul class="mw-languages-major">';
+    foreach($hwLangConfig["languages"] as $langCode => $langName) {
+      // If it's NOT in Babylist
+      if(!in_array($langCode, $hwLangConfig['settings']['baby'])) {
 
-				$languages .= '<li>';
-				$languages .= '<a href="/'.$langCode.'">'.$langName.'</a>';
-				$languages .= '<span class="hw-languages-articles">(' . sprintf('%d articles', 0) . ')</span>';
-				$languages .= '</li>';
-			}
-		}
-		$languages .= '</ul>';
+        $languages .= '<li>';
+        $languages .= '<a href="/'.$langCode.'">'.$langName.'</a>';
+        $languages .= '<span class="hw-languages-articles">' . sprintf('%d articles', 0) . '</span>';
+        $languages .= '</li>';
+      }
+    }
+    $languages .= '</ul>';
 
 
-		$languages .= '</div>'; //mw-languages
+    // Baby wikis
+    $languages .= '<h4 class="mw-languages-label-minor">Baby wikis</h4>';
+    $languages .= '<ul class="mw-languages-minor">';
+    foreach($hwLangConfig["languages"] as $langCode => $langName) {
+      // If it IS in Babylist
+      if(in_array($langCode, $hwLangConfig['settings']['baby'])) {
 
-  	return $languages;
+        $languages .= '<li>';
+        $languages .= '<a href="/'.$langCode.'">'.$langName.'</a>';
+        $languages .= '<span class="hw-languages-articles">(' . sprintf('%d articles', 0) . ')</span>';
+        $languages .= '</li>';
+      }
+    }
+    $languages .= '</ul>';
+
+
+    $languages .= '</div>'; //mw-languages
+
+    return $languages;
   }
 
 }
